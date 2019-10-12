f9412da08994d86386b5f9269ce414cd1b8bfc2d
御立田航平 <kohei.mitateda@gmail.com>
2019-10-12 04:20:12 +0000

    20191012

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 15}
   validates :email, presence: true
-  validates :password, length: {minimum: 8, maximum: 32}
+  validates :password, length: {minimum: 8, maximum: 32}, format: {with:/\A[A-Za-z0-9]+\z/}
   validates :password_confirmation, length: {minimum: 8, maximum: 32}
   
   has_secure_password
